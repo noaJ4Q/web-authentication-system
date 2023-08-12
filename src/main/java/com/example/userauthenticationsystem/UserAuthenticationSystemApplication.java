d37554aa6489228e1d504b0287e26eb669140369
noe <noe.jaraq@gmail.com>
2023-08-12 13:00:01 -0500

    first commit

---
+++ src/main/java/com/example/userauthenticationsystem/UserAuthenticationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.userauthenticationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenticationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAuthenticationSystemApplication.class, args);
+    }
+
+}
