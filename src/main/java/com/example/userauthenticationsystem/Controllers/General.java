1ddd30eddd0b60d205184a6dae70a7cd51331fa1
noe <noe.jaraq@gmail.com>
2023-08-12 22:17:54 -0500

    signup view added and structure advanced

---
+++ src/main/java/com/example/userauthenticationsystem/Controllers/General.java
@@ -0,0 +1,17 @@
+package com.example.userauthenticationsystem.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class General {
+    @GetMapping("/")
+    public String signinPage(){
+        return "signin";
+    }
+
+    @GetMapping("/signup")
+    public String signupPage(){
+        return "signup";
+    }
+}
