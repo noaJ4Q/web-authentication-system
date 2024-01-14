6ad811df41dabd5a268723a24c7c22ff3f438b99
noe <noe.jaraq@gmail.com>
2024-01-14 17:37:15 -0500

    send email to recover pass

--- src/main/java/com/example/userauthenticationsystem/Controllers/GeneralController.java
+++ src/main/java/com/example/userauthenticationsystem/Controllers/GeneralController.java
@@ -4,7 +4,9 @@ import com.example.userauthenticationsystem.Entities.Credentials;
 import com.example.userauthenticationsystem.Entities.User;
 import com.example.userauthenticationsystem.Repositories.CredentialsRepository;
 import com.example.userauthenticationsystem.Repositories.UserRepository;
+import com.example.userauthenticationsystem.Services.EmailService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
@@ -23,20 +25,22 @@ public class GeneralController {
 
     private final UserRepository userRepository;
     private final CredentialsRepository credentialsRepository;
-
+    private final EmailService emailService;
     public GeneralController(UserRepository userRepository,
-                             CredentialsRepository credentialsRepository) {
+                             CredentialsRepository credentialsRepository,
+                             EmailService emailService) {
         this.userRepository = userRepository;
         this.credentialsRepository = credentialsRepository;
+        this.emailService = emailService;
     }
 
     @GetMapping(value = {"", "/"})
-    public String signinPage(){
+    public String signinPage() {
         return "signin";
     }
 
     @GetMapping("/signup")
-    public String signupPage(){
+    public String signupPage() {
         return "signup";
     }
 
@@ -44,16 +48,16 @@ public class GeneralController {
     public String saveUser(@Valid User user, BindingResult bindingResult,
                            RedirectAttributes attributes,
                            @RequestParam("password") String password,
-                           @RequestParam("confirmPassword") String confirmPassword){
+                           @RequestParam("confirmPassword") String confirmPassword) {
 
         boolean validPasswords = validatePassword(password, confirmPassword);
 
-        if (bindingResult.hasErrors() && !validPasswords){
-            System.out.println("Validation error: "+bindingResult);
+        if (bindingResult.hasErrors() && !validPasswords) {
+            System.out.println("Validation error: " + bindingResult);
             return "signup";
-        }
-        else{
-            attributes.addFlashAttribute("signup", true);
+        } else {
+            attributes.addFlashAttribute("toast", true);
+            attributes.addFlashAttribute("toastText", "Account created successfully");
             user.setState(1);
             user.setRole(0);
             userRepository.save(user);
@@ -64,11 +68,31 @@ public class GeneralController {
     }
 
     @GetMapping(value = {"/forgot", "/forgot/{token}"})
-    public String forgotPage(@PathVariable(name = "token", required = false) String token){
+    public String forgotPage(@PathVariable(name = "token", required = false) String token) {
+
         return "forgot";
     }
 
-    private void saveCredentials(User user, String password){
+    @PostMapping("/forgot")
+    public String receiveEmail(@RequestParam("email") String email,
+                               RedirectAttributes attributes) {
+
+        String toastText;
+        String subject = "Recover account";
+        String emailText = "Hi,\n" +
+                "You have requested to reset your password. Please enter in the following lonk to restore your passsword:\n" +
+                "Ignore this email if you do remeber your password, or you have not made this request.";
+
+        emailService.sendEmail(email, subject, emailText);
+        toastText = "Email sended to: "+email;
+        //toastText = "Error sending email to: "+email;
+
+        attributes.addFlashAttribute("toast", true);
+        attributes.addFlashAttribute("toastText", toastText);
+        return "redirect:/";
+    }
+
+    private void saveCredentials(User user, String password) {
         Credentials credentials = new Credentials();
         String encryptedPassword = new BCryptPasswordEncoder().encode(password);
 
@@ -79,7 +103,7 @@ public class GeneralController {
         credentialsRepository.save(credentials);
     }
 
-    private boolean validatePassword(String password, String confirmPassword){
+    private boolean validatePassword(String password, String confirmPassword) {
         return !password.equals("") && !confirmPassword.equals("") && password.equals(confirmPassword);
     }
 }
